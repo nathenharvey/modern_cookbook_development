633f01e1a7698f1365d569124ebe144f43b38820
Nathen Harvey <nharvey@chef.io>
2017-01-23 10:55:22 -0500

    Chef Development Kit 1.1.16
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ one_zero_three/README.md
@@ -0,0 +1,4 @@
+# one_zero_three
+
+TODO: Enter the cookbook description here.
+
---
+++ one_zero_three/metadata.rb
@@ -0,0 +1,15 @@
+name 'one_zero_three'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures one_zero_three'
+long_description 'Installs/Configures one_zero_three'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/one_zero_three/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/one_zero_three' if respond_to?(:source_url)
