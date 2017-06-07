e8173a15d171ce660167f9e390cad042ad7e89be
Nathen Harvey <nharvey@chef.io>
2017-06-07 14:00:14 -0600

    Chef Development Kit 1.4.3
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ one_four_three/README.md
@@ -0,0 +1,4 @@
+# one_four_three
+
+TODO: Enter the cookbook description here.
+
---
+++ one_four_three/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: one_four_three
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
