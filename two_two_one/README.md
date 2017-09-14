3201bd3330f32a13c48d12383ae14959962a83a7
Nathen Harvey <nharvey@chef.io>
2017-09-14 17:33:24 +0530

    Chef DK 2.2.1
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ two_two_one/README.md
@@ -0,0 +1,4 @@
+# two_two_one
+
+TODO: Enter the cookbook description here.
+
---
+++ two_two_one/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: two_two_one
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
