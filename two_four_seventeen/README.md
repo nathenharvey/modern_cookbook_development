c219628a11d8dc6096d47c49825f7c2ed658c5c9
Nathen Harvey <nharvey@chef.io>
2017-11-29 11:52:50 -0500

    Update for Chef DK 2.4.17
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ two_four_seventeen/README.md
@@ -0,0 +1,4 @@
+# two_four_seventeen
+
+TODO: Enter the cookbook description here.
+
---
+++ two_four_seventeen/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: two_four_seventeen
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
