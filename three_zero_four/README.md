e8122ce0d6e93a141bbbe8acab4605e1bb1878d0
Nathen Harvey <nharvey@chef.io>
2018-04-08 22:03:46 -0400

    Chef-DK 3.0.4
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ three_zero_four/README.md
@@ -0,0 +1,4 @@
+# three_zero_four
+
+TODO: Enter the cookbook description here.
+
---
+++ three_zero_four/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: three_zero_four
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
