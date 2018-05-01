ca89877d0b9ac6ee8ee05ec8d7c0fe9652722055
Nathen Harvey <nharvey@chef.io>
2018-05-01 09:10:18 -0400

    Chef DK 2.5.3
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ two_five_three/README.md
@@ -0,0 +1,4 @@
+# two_five_three
+
+TODO: Enter the cookbook description here.
+
---
+++ two_five_three/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: two_five_three
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
